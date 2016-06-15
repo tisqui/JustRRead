b6dcea4fff5de161280b719cdad826717a147eaf
tisqui <squiiirrel@gmail.com>
2016-06-14 23:24:18 -0700

    added dialogs style, buttons style

--- app/src/main/java/com/squirrel/justrread/activities/BaseActivity.java
+++ app/src/main/java/com/squirrel/justrread/activities/BaseActivity.java
@@ -1,10 +1,10 @@
 package com.squirrel.justrread.activities;
 
-import android.app.AlertDialog;
 import android.content.Context;
 import android.content.DialogInterface;
 import android.os.Bundle;
 import android.support.v4.app.Fragment;
+import android.support.v7.app.AlertDialog;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
 import android.util.Log;
